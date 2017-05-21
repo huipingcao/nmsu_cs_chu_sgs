c0d4a9a80cecec7f500f3ebeca0ccd11dd9f0981
Chuan Hu <chuanhu90@gmail.com>
2017-05-21 13:13:52 -0600

    Update cmd_example.sh

--- cmd/cmd_example.sh
+++ cmd/cmd_example.sh
@@ -13,9 +13,10 @@ for Znum in 10 # change number of topics here
 do
     for m in 0 1 8 9 # loop through all comparison method
     do
-        # IMPORTANT: change the data path to the local data path on your machine.
-        # output log and parameters are also writtent to this path.
+
+        # output log and parameters are writtent to this path.
         sampler_id=/data/chu/lda-data/nomad_lda_data/output/$DATA_SET-$m-Z$Znum-${DATE}
+        # IMPORTANT: change the data path to the local data path on your machine.
         ../splda $Znum $iteration /data/chu/lda-data/nomad_lda_data/$DATA_SET $m $sampler_id
     done
 done
