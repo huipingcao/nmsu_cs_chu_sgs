ec5fb8d9a8cb5848645c9a8689f4a7d54bb56dcb
Chuan Hu <chuanhu90@gmail.com>
2017-05-21 13:09:06 -0600

    Add files via upload

---
+++ cmd/cmd_example.sh
@@ -0,0 +1,19 @@
+#PBS -l nodes=1:ppn=1
+#PBS -l walltime=24000:00:00
+
+iteration=100
+DATE=20170520
+DATA_SET=nytimes #nips nytimes pubmed 
+
+# solver: m=0  Normal LDA
+# solver: m=1  Sparse LDA
+# solver: m=8  Alias LDA
+# solver: m=9  F+LDA - word-by-word
+for Znum in 10 # change number of topics here
+do
+    for m in 0 1 8 9 # loop through all comparison method
+    do
+        sampler_id=/data/chu/lda-data/nomad_lda_data/output/$DATA_SET-$m-Z$Znum-${DATE}
+        ../splda $Znum $iteration /data/chu/lda-data/nomad_lda_data/$DATA_SET $m $sampler_id
+    done
+done
